2d88a138670fcdedb236bf0a04b98da9cd3e2ff0
Tuan Hiep <ithadev.nguyen@gmail.com>
2025-04-29 16:31:32 +0700

    feat: add terrform gke

---
+++ tfs/gke/gcloud/variables.tf
@@ -0,0 +1,17 @@
+variable "project_name" {
+  type        = string
+  default     = "default_project_name"
+  description = "Project name variable"
+}
+
+variable "region" {
+  type        = string
+  default     = "default_region"
+  description = "Region variable"
+}
+
+variable "zone" {
+  type        = string
+  default     = "default_zone"
+  description = "Zone variable"
+}
